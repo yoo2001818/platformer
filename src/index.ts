960fa7fdf385e99575d5dfd714620c1ce93fa258
Duknam Yoo <yoo2001818@gmail.com>
2021-06-12 14:30:32 +0900

    Update eslint config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import testModule from './testModule';
-import { mat4 } from 'gl-matrix';
+import {mat4} from 'gl-matrix';
 
 const mat = mat4.create();
 
-console.log("Hello World!" + testModule);
 console.log(mat);
