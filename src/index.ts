13b5cb178c85917a1695619fe776274db9a17aab
Duknam Yoo <yoo2001818@gmail.com>
2021-06-12 14:08:52 +0900

    Add vscode support

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+import testModule from './testModule';
+import { mat4 } from 'gl-matrix';
+
+const mat = mat4.create();
+
+console.log("Hello World!" + testModule);
+console.log(mat);
