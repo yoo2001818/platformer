04eb71da4249e276f308d8aa240868a7ce6360e2
Duknam Yoo <yoo2001818@gmail.com>
2021-06-19 23:57:34 +0900

    Add createArrayComponent

---
+++ src/core/components/createArrayComponent.ts
@@ -0,0 +1,57 @@
+import type {Entity} from '../Entity';
+
+import {Component} from './Component';
+
+export function createArrayComponent(
+  dimensions: number,
+): Component<Float32Array> {
+  let index: number | null = null;
+  function getHashCode(value: Float32Array | null): number {
+    return value == null ? 0 : 1;
+  }
+  function validateHash(
+    entity: Entity,
+    prevValue: Float32Array | null,
+    nextValue: Float32Array | null,
+  ): void {
+    const prevHash = getHashCode(prevValue);
+    const nextHash = getHashCode(nextValue);
+    if (prevHash !== nextHash) {
+      entity.float();
+    }
+  }
+  return {
+    getIndex() {
+      return index;
+    },
+    register(storeVal, indexVal) {
+      index = indexVal;
+    },
+    unregister() {
+      index = null;
+    },
+    get(entity) {
+      return (entity.componentMap[index!] ?? null) as Float32Array | null;
+    },
+    set(entity, value) {
+      validateHash(entity, entity.componentMap[index!] as Float32Array | null, value);
+      entity.componentMap[index!] = value;
+    },
+    delete(entity) {
+      validateHash(entity, entity.componentMap[index!] as Float32Array | null, null);
+      entity.componentMap[index!] = null;
+    },
+    getHashCode(value) {
+      return value == null ? 0 : 1;
+    },
+    initChunk(chunk, value) {
+
+    },
+    getChunk(chunk, offset) {
+
+    },
+    setChunk(chunk, offset, value) {
+
+    },
+  };
+}
