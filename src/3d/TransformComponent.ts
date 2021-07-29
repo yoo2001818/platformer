398cb7aca57b9b3497cecbe86aca18fa9e4e7d8a
Duknam Yoo <yoo2001818@gmail.com>
2021-07-29 23:53:24 +0900

    Add TransformComponent

---
+++ src/3d/TransformComponent.ts
@@ -0,0 +1,7 @@
+import {ObjectComponent} from '../core/components';
+
+import {Transform} from './Transform';
+
+export class TransformComponent extends ObjectComponent<Transform> {
+  // TODO: EntityGroup
+}
