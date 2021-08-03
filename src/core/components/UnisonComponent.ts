744e2fd556a605b4f8b16c93cb5e08f99b2b9feb
Duknam Yoo <yoo2001818@gmail.com>
2021-08-04 02:19:53 +0900

    Implement UnisonComponent

--- src/core/components/UnisonComponent.ts
+++ src/core/components/UnisonComponent.ts
@@ -1,16 +1,22 @@
 import type {Entity} from '../Entity';
+import type {EntityChunk} from '../EntityChunk';
 import type {EntityStore} from '../EntityStore';
 
 import {Component} from './Component';
 
-export class ObjectComponent<
+export class UnisonComponent<
   TReadValue,
   TWriteValue extends TReadValue = TReadValue
 > implements Component<TReadValue, TWriteValue> {
 
   index: number | null = null;
 
-  constructor() {
+  allocatedIds: Map<string, number> = new Map();
+
+  _getSignature: (value: TReadValue) => string;
+
+  constructor(getSignature: (value: TReadValue) => string) {
+    this._getSignature = getSignature;
   }
 
   _validateHash(
@@ -60,7 +66,28 @@ export class ObjectComponent<
   }
 
   getHashCode(value: TWriteValue | null): number {
-    return value == null ? 0 : 1;
+    if (value == null) {
+      return 0;
+    }
+    const signature = this._getSignature(value);
+    let id = this.allocatedIds.get(signature);
+    if (id != null) {
+      return id;
+    }
+    id = this.allocatedIds.size + 1;
+    this.allocatedIds.set(signature, id);
+    return id;
   }
 
+  initChunk(chunk: EntityChunk, value: TReadValue | null): void {
+    chunk._setRawMap(this, value);
+  }
+
+  getChunk(chunk: EntityChunk, offset: number): TReadValue | null {
+    return chunk._getRawMap(this);
+  }
+
+  setChunk(chunk: EntityChunk, offset: number, value: TWriteValue): void {
+    // Do nothing. :/
+  }
 }
