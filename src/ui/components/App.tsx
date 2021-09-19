465818d54265e4529059ca7e1aa3731a52a16f63
Duknam Yoo <yoo2001818@gmail.com>
2021-09-19 22:21:16 +0900

    Add engine in React

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
+
+import {Engine} from '../../core/Engine';
+
+import {EngineProvider} from './EngineContext';
 
 export function App(): React.ReactElement {
+  const engine = useRef<Engine | null>(null);
+  if (engine.current == null) {
+    engine.current = new Engine();
+  }
   return (
-    <div>
-      Hello, world!
-    </div>
+    <EngineProvider engine={engine.current}>
+      <div>
+        { JSON.stringify(engine.current, null, 2) }
+      </div>
+    </EngineProvider>
   );
 }
