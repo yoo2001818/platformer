479828ff50da55c09eb289d69294da18992ee09f
Duknam Yoo <yoo2001818@gmail.com>
2021-09-19 17:42:45 +0900

    Add Engine class

---
+++ src/core/Engine.ts
@@ -0,0 +1,47 @@
+import {EntityStore} from './EntityStore';
+
+export const BEFORE_UPDATE_PHASE = 0;
+export const UPDATE_PHASE = 1;
+export const AFTER_UPDATE_PHASE = 2;
+export const RENDER_PHASE = 3;
+export const AFTER_RENDER_PHASE = 4;
+
+export interface SystemFunction {
+  (deltaTime?: number): void;
+}
+
+export class Engine {
+  entityStore: EntityStore;
+  systems: Set<SystemFunction>[];
+  resources: Map<string, any>;
+
+  constructor() {
+    this.entityStore = new EntityStore();
+    this.systems = [];
+    this.resources = new Map();
+
+    const sorter = () => this.entityStore.sort();
+    this.registerSystem(AFTER_UPDATE_PHASE, sorter);
+  }
+
+  registerSystem(phase: number, callback: SystemFunction): void {
+    if (this.systems[phase] == null) {
+      this.systems[phase] = new Set();
+    }
+    this.systems[phase].add(callback);
+  }
+
+  setResource(name: string, value: any): void {
+    this.resources.set(name, value);
+  }
+
+  getResource<T>(name: string): T {
+    return this.resources.get(name) as T;
+  }
+
+  update(deltaTime?: number): void {
+    this.systems.forEach((set) => {
+      set.forEach((callback) => callback(deltaTime));
+    });
+  }
+}
---
+++ src/ui/hooks/useEntity.ts
@@ -0,0 +1,5 @@
+import {Entity} from '../../core/Entity';
+
+export function useEntity(entity: Entity): Entity {
+  return entity;
+}
