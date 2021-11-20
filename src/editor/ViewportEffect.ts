e7c37cb829aedfe22f27a805ef565ccd43f31e5e
Duknam Yoo <yoo2001818@gmail.com>
2021-11-20 23:58:20 +0900

    Add Effect traversal

--- src/editor/ViewportEffect.ts
+++ src/editor/ViewportEffect.ts
@@ -6,9 +6,11 @@ import {Viewport} from './Viewport';
 
 export class ViewportEffect implements GizmoEffect {
   viewport: Viewport;
+  prevEffects: GizmoEffect[];
 
   constructor(viewport: Viewport) {
     this.viewport = viewport;
+    this.prevEffects = [];
   }
 
   bind(renderer: Renderer): void {
@@ -19,6 +21,9 @@ export class ViewportEffect implements GizmoEffect {
     const modeModel = this.viewport.engine!.getModel<ModeModel>('mode');
     const effects = modeModel.mode.getEffects();
     // Bind and dispose effects
+    effects.forEach((effect) => {
+      //
+    });
   }
 
   dispose(): void {
