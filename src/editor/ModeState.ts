d82eaca536d1ed02bd3e8acdb99e485eaec7610d
Duknam Yoo <yoo2001818@gmail.com>
2021-11-28 23:57:51 +0900

    Add Constructable type

--- src/editor/ModeState.ts
+++ src/editor/ModeState.ts
@@ -1,6 +1,7 @@
 import {vec2, vec3} from 'gl-matrix';
 
 import {EntityHandle} from '../core/EntityHandle';
+import {GizmoEffect} from '../render/effect/GizmoEffect';
 
 export interface EditorState {
   mode: 'default' | 'translate' | 'scale' | 'rotate';
@@ -13,8 +14,12 @@ export interface TranslateState {
   origin: vec2;
 }
 
+export interface Constructable<T> {
+  new(): T;
+}
+
 export interface RenderNode {
   type: 'gizmo';
-  component: unknown;
-  props: unknown;
+  component: Constructable<GizmoEffect>;
+  props: {[key: string]: unknown;};
 }
