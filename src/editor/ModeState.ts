297ac37fb027928433ef6a1faa0408fc11bda28b
Duknam Yoo <yoo2001818@gmail.com>
2021-12-02 23:59:41 +0900

    Add EditorStateHandler

--- src/editor/ModeState.ts
+++ src/editor/ModeState.ts
@@ -23,3 +23,8 @@ export interface RenderNode<TProps> {
   component: Constructable<GizmoEffect<TProps>>;
   props: TProps;
 }
+
+export interface EditorStateHandler {
+  render(): RenderNode<unknown>[];
+  processEvent(type: string, event: Event): void;
+}
