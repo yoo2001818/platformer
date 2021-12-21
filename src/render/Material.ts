0d60f1153434d0beb21bd79207d14bbbe29836e0
Duknam Yoo <yoo2001818@gmail.com>
2021-12-21 23:58:54 +0900

    Add MaterialV2 type

--- src/render/Material.ts
+++ src/render/Material.ts
@@ -1,6 +1,8 @@
 import {EntityChunk} from '../core/EntityChunk';
 
 import {GLGeometry} from './gl/GLGeometry';
+import {GLShader} from './gl/GLShader';
+import {DrawOptions} from './gl/types';
 import {Renderer} from './Renderer';
 import {ShadowPipeline} from './shadow/ShadowPipeline';
 
@@ -15,4 +17,17 @@ export interface Material {
   ): void;
   render(chunk: EntityChunk, geometry: GLGeometry, renderer: Renderer): void;
   dispose(): void;
+}
+
+// In order to merge renderShadow and render, we can separate it in two steps
+
+interface MaterialV2 {
+  renderVertex(
+    chunk: EntityChunk,
+    geometry: GLGeometry,
+    renderer: Renderer,
+    onGetShader: (id: string, vert: string) => GLShader,
+    onDraw: (options: DrawOptions) => void,
+  ): void;
+  render(chunk: EntityChunk, geometry: GLGeometry, renderer: Renderer): void;
 }
