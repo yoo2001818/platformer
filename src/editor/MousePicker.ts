80558ec526db7fe2402282b65a245b5de32abc2d
Duknam Yoo <yoo2001818@gmail.com>
2022-01-02 23:40:30 +0900

    Improve MousePicker to allow non-mesh entities

--- src/editor/MousePicker.ts
+++ src/editor/MousePicker.ts
@@ -1,5 +1,9 @@
+import {TransformComponent} from '../3d/TransformComponent';
 import {Entity} from '../core/Entity';
+import {quad} from '../geom/quad';
+import {GLArrayBuffer} from '../render/gl/GLArrayBuffer';
 import {GLFrameBuffer} from '../render/gl/GLFrameBuffer';
+import {GLGeometry} from '../render/gl/GLGeometry';
 import {GLRenderBuffer} from '../render/gl/GLRenderBuffer';
 import {GLShader} from '../render/gl/GLShader';
 import {GLTexture2D} from '../render/gl/GLTexture2D';
@@ -10,6 +14,7 @@ export class MousePicker {
   colorTex: GLTexture2D;
   depthTex: GLRenderBuffer;
   renderFb: GLFrameBuffer;
+  instancedBuffer: GLArrayBuffer;
 
   constructor(renderer: Renderer) {
     this.renderer = renderer;
@@ -31,17 +36,19 @@ export class MousePicker {
       color: this.colorTex,
       depthStencil: this.depthTex,
     });
+    this.instancedBuffer = new GLArrayBuffer(null, 'stream');
   }
 
   dispose(): void {
     this.colorTex.dispose();
     this.depthTex.dispose();
     this.renderFb.dispose();
+    this.instancedBuffer.dispose();
   }
 
   render(): void {
     const {renderer} = this;
-    const {glRenderer, pipeline} = renderer;
+    const {glRenderer, pipeline, entityStore} = renderer;
     const width = glRenderer.getWidth();
     const height = glRenderer.getHeight();
     this.colorTex.updateSize(width, height);
@@ -52,6 +59,7 @@ export class MousePicker {
       undefined,
       [1, 1, 1, 1],
     );
+    // Render all meshes on the screen first
     pipeline.renderVertex(
       (id, onCreate) => renderer.getResource(`mousepick~${id}`, () => {
         const block = onCreate();
@@ -83,6 +91,81 @@ export class MousePicker {
         });
       },
     );
+    // Then, render all non-mesh entities (perferably with instancing)
+    const transformComp =
+      entityStore.getComponent<TransformComponent>('transform')!;
+    const nonmeshShader = renderer.getResource('mousepick-nonmesh-s', () => {
+      return new GLShader(
+        /* glsl */`
+          precision highp float;
+
+          attribute vec3 aPosition;
+          attribute vec4 aInstanced;
+
+          varying float vEntityId;
+
+          uniform mat4 uView;
+          uniform mat4 uProjection;
+          uniform vec2 uScale;
+
+          void main() {
+            vEntityId = aInstanced.w;
+            mat4 mvp = uProjection * uView;
+            vec4 pos = mvp * vec4(aInstanced.xyz, 1.0);
+            pos.xy += aPosition.xy * uScale * pos.w;
+            gl_Position = pos;
+          }
+        `,
+        /* glsl */`
+          precision highp float;
+
+          varying float vEntityId;
+          
+          const vec4 encTable = vec4(1.0, 255.0, 65025.0, 16581375.0);
+
+          void main() {
+            vec4 enc = vec4(mod(floor(vec4(vEntityId + 0.001) / encTable), 255.0) / 255.0);
+            gl_FragColor = enc;
+          }
+        `,
+      );
+    });
+    const nonmeshGeom = renderer.getResource('mousepick-nonmesh-g', () => {
+      return new GLGeometry(quad());
+    });
+    entityStore.forEachChunkWith(['transform'], (chunk) => {
+      if (chunk.has('mesh')) {
+        return;
+      }
+      const buffer = new Float32Array(chunk.size * 4);
+      let index = 0;
+      for (let i = 0; i < chunk.maxOffset; i += 1) {
+        const entity = chunk.getAt(i);
+        if (entity == null) {
+          continue;
+        }
+        const transform = entity.get(transformComp);
+        buffer.set(transform!.getPositionWorld(), index * 4);
+        buffer[index * 4 + 3] = entity.handle.id;
+        index += 1;
+      }
+      this.instancedBuffer.set(buffer);
+      nonmeshShader.bind(glRenderer);
+      nonmeshShader.setAttribute('aInstanced', {
+        buffer: this.instancedBuffer,
+        divisor: 1,
+      });
+      glRenderer.draw({
+        geometry: nonmeshGeom,
+        shader: nonmeshShader,
+        frameBuffer: this.renderFb,
+        uniforms: {
+          ...cameraUniforms,
+          uScale: [8 / width, 8 / height],
+        },
+        primCount: chunk.size,
+      });
+    });
   }
 
   getId(x: number, y: number): number {
