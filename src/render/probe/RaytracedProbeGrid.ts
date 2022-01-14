4a748d607eeabe6a68089daba7ea7a8a297e9d4d
Duknam Yoo <yoo2001818@gmail.com>
2022-01-14 23:58:50 +0900

    Add running raytrace in probe grid

--- src/render/probe/RaytracedProbeGrid.ts
+++ src/render/probe/RaytracedProbeGrid.ts
@@ -1,9 +1,14 @@
+import {quad} from '../../geom/quad';
 import {getBVHTexture, getLightTexture} from '../bvhResource';
 import {GLFrameBuffer} from '../gl/GLFrameBuffer';
+import {GLGeometry} from '../gl/GLGeometry';
+import {GLShader} from '../gl/GLShader';
 import {GLTexture2D} from '../gl/GLTexture2D';
 import {Renderer} from '../Renderer';
 
 import {ProbeGrid, ProbeGridOptions} from './ProbeGrid';
+
+const LIGHT_QUAD = new GLGeometry(quad());
 
 export class RaytracedProbeGrid implements ProbeGrid {
   options: ProbeGridOptions;
@@ -89,10 +94,46 @@ export class RaytracedProbeGrid implements ProbeGrid {
   }
 
   prepare(renderer: Renderer): void {
+    const {glRenderer} = renderer;
     this._prepareTexture(renderer);
     const bvhTexture = getBVHTexture(renderer);
     bvhTexture.update();
     const lightTexture = getLightTexture(renderer);
     lightTexture.update();
+    const rtShader = renderer.getResource(`rtprobe~raytrace`, () => {
+      return new GLShader(
+        /* glsl */`
+          #version 100
+          precision highp float;
+
+          attribute vec3 aPosition;
+
+          varying vec2 vPosition;
+
+          void main() {
+            vPosition = aPosition.xy;
+            gl_Position = vec4(vPosition, 1.0, 1.0);
+          }
+        `,
+        /* glsl */`
+          #version 100
+          precision highp float;
+          precision highp sampler2D;
+
+          varying vec2 vPosition;
+
+          void main() {
+            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+          }
+        `,
+      );
+    });
+    // Run raytrace
+    glRenderer.draw({
+      shader: rtShader,
+      geometry: LIGHT_QUAD,
+      uniforms: {},
+      frameBuffer: this.rtFrameBuffer!,
+    });
   }
 }
