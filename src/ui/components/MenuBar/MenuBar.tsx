6e5a5754c59d0e72969ad0207cee66ba34a9c5bd
Duknam Yoo <yoo2001818@gmail.com>
2022-03-01 23:59:58 +0900

    Adjust import statement

--- src/ui/components/MenuBar/MenuBar.tsx
+++ src/ui/components/MenuBar/MenuBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { COLORS } from '../../styles';
+
+import {COLORS} from '../../styles';
 
 export interface MenuBarProps {
   className?: string;
