886db9fef1cecd7dc4ee61e50694df9ff5295bde
Duknam Yoo <yoo2001818@gmail.com>
2022-03-21 23:59:22 +0900

    Add file commands

---
+++ src/editor/commands/file.ts
@@ -0,0 +1,13 @@
+import {Engine} from '../../core/Engine';
+
+export function newFile(engine: Engine): void {
+  engine.entityStore.deleteAll();
+}
+
+export function loadFile(engine: Engine): void {
+
+}
+
+export function saveFile(engine: Engine): void {
+  
+}
