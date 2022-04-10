2bc5dfd68d912a9c7e2a05a3ac26c4ddd508f8b6
Duknam Yoo <yoo2001818@gmail.com>
2022-04-10 17:17:54 +0900

    Implement ResourceMap

--- src/core/serialization/ResourceMap.ts
+++ src/core/serialization/ResourceMap.ts
@@ -8,20 +8,41 @@ export interface ResourceMapDescriptor<T extends ResourceMapComponent> {
 }
 
 export class ResourceMap {
+  entries: Map<string, ResourceMapComponent>;
+  entriesJson: Map<string, unknown>;
 
   constructor() {
-    // TODO
+    this.entries = new Map();
+    this.entriesJson = new Map();
   }
 
   fromJSON(json: unknown): void {
-
+    if (typeof json === 'object') {
+      const jsonObj = json as {[key: string]: unknown;};
+      this.entriesJson = new Map(Object.entries(jsonObj));
+    }
   }
 
-  get<T>(descriptor: ResourceMapDescriptor<T>): T {
-
+  get<T extends ResourceMapComponent>(descriptor: ResourceMapDescriptor<T>): T {
+    const oldEntry = this.entries.get(descriptor.name);
+    if (oldEntry != null) {
+      return oldEntry as T;
+    }
+    let jsonObj: unknown;
+    if (this.entriesJson.has(descriptor.name)) {
+      jsonObj = this.entriesJson.get(descriptor.name);
+      this.entriesJson.delete(descriptor.name);
+    }
+    const newEntry = descriptor.create(jsonObj);
+    this.entries.set(descriptor.name, newEntry);
+    return newEntry;
   }
 
-  toJSON(): void {
-
+  toJSON(): unknown {
+    return Object.fromEntries(
+      Array.from(this.entries.entries()).map(([key, entry]) => {
+        return [key, entry.toJSON()];
+      }),
+    );
   }
 }
